70b0b502ccf4ff7f411a111d896b298a4954ffaf
Shamay Osipov <shamayagaron@gmail.com>
2014-04-06 18:37:24 -0400

    Stack2

--- 12-Stack/MyStack.java
+++ 12-Stack/MyStack.java
@@ -1,52 +1,65 @@
 public class MyStack {
 
-    private Node top;
+    // private Node top;
     private int numElts;
+    private int size;
+    private Node[] elements;
+
 
     // contructor
     public MyStack(){
-	top = null;
+	// top = null;
+	elements = new Node[10];
 	numElts = 0;
+    }
+
+    public void grow(){
+	if (numElts >= size){
+	    Node[] temp = new Node[size*2];
+	    for(int x = 0; x < size ; x++){
+		temp[x] = elements[x];
+	    }
+	    elements = temp;
+	    size = size * 2;
+	}
     }
 
     // push
     public void push(String s){
 	Node n = new Node(s);
-	n.setNext(top);
-	top = n;
+	grow();
+	elements[numElts] = n;
 	numElts++;
     }
     
     // pop
     public String pop(){
-	String temp = top.getData();
-	top = top.getNext();
+	String temp = elements[numElts - 1].getData();
+	elements[numElts - 1] = null;
 	numElts--;
 	return temp;
     }
 
     // peek
     public String peek(){
-	return top.getData();
+	return elements[numElts - 1].getData();
     }
 
 
     // isEmpty
     public Boolean isEmpty(){
-	return top == null;
+	return elements[numElts - 1] == null;
     }
     
-    public getSize(){
+    public int getSize(){
 	return numElts;
     }
 	
     // toString
     public String toString(){
 	String tmp = "";
-	Node currentNode = top;
-	while (currentNode != null){
-	    tmp += currentNode.getData() + ", ";
-	    currentNode = currentNode.getNext();
+	for(int x = 0; x<numElts; x++){
+	    tmp += elements[x].getData() + ", ";
 	}
 	return tmp;
     }
